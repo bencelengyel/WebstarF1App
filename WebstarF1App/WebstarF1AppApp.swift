04b4b6dfa831896c9a66dc314a930befb23ecea0
Bence Lengyel <grids_resorts_1n@icloud.com>
2026-03-20 16:02:03 +0100

    Initial Commit

---
+++ WebstarF1App/WebstarF1AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebstarF1AppApp.swift
+//  WebstarF1App
+//
+//  Created by Bence on 2026.03.20.
+//
+
+import SwiftUI
+
+@main
+struct WebstarF1AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
